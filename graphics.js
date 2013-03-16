11615ed41aac7f7fc203e62896c0a32f8a3756d0
Ian McLaughlin <ian.p.mclaughlin@gmail.com>
2013-03-16 15:32:56 -0400

    adding graphics file

---
+++ graphics.js
@@ -0,0 +1,21 @@
+function initWorld() {
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+    var renderer = new THREE.CanvasRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+    var geometry = new THREE.CubeGeometry(1,1,1);
+    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    var cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+
+    var components = {};
+    components.scene = scene;
+    components.camera = camera;
+    components.renderer = renderer;
+    components.cube = cube;
+    components.renderer = renderer;
+
+    return components;
+}
