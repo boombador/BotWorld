81673bc66f8b0a3c2053954b983879c63da20e48
Ian McLaughlin <ian.p.mclaughlin@gmail.com>
2013-03-16 13:10:10 -0400

    developing the player class

--- game.js
+++ game.js
@@ -5,36 +5,24 @@ var engine = {
 };
 
 function Player(x, y) {
+    // current coordinates
 	this.x = x;
 	this.y = y;
+    // moveTo coordinates
+    this.mx = x;
+    this.my = y;
+
+    // mesh that should be updated for Three.js render calls
+    this.body = null;
 }
-
-function init() {
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-    var renderer = new THREE.CanvasRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement );
-    var geometry = new THREE.CubeGeometry(1,1,1);
-    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    var cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
-
-    var components = {};
-    components.scene = scene;
-    components.camera = camera;
-    components.renderer = renderer;
-    components.cube = cube;
-    components.renderer = renderer;
-
-    return components;
-}
-
-// deprecated
-Player.prototype.draw = function() {
-	var loc = "cell" + this.x + "," + this.y;
-	document.getElementById(loc).className += " player";
-};
 
 engine.player.x = 3;
+
+Player.prototype.affix = function(mesh) {
+    this.body = mesh;
+}
+
+Player.prototype.moveTo(x, y) {
+    this.mx = x;
+    this.my = y;
+}
