0e22e1a83adab4c97a3bcaf27f0cbd5759bdebca
Ian McLaughlin <ian.p.mclaughlin@gmail.com>
2013-07-02 20:47:04 -0400

    working acceleration, vector math ftw

--- js/Ship.js
+++ js/Ship.js
@@ -1,54 +1,40 @@
+
 function Ship() {
     this.geoBody = new THREE.CubeGeometry(1,1,1);
     this.matBody = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
     this.body = new THREE.Mesh( this.geoBody, this.matBody );
 
-    this.rot = 1.57079633;
+    this.maxSpeed = .2;
+    this.speed = 0;
+    this.thrustForce = .0001;
+    this.dir = new THREE.Vector3( 0, 0, 1 );
+    this.vel = new THREE.Vector3( 0, 0, 0 );
 
     return this;
 }
 
-Ship.prototype.yaw = function(rad){
-    this.rot += rad;
-    this.body.rotation.y = this.rot;
+Ship.prototype.yaw = function(angle){
+    var up = new THREE.Vector3( 0, 1, 0 );
+    var rotation = new THREE.Matrix4().makeRotationAxis( up, angle );
+
+    this.dir.applyMatrix4( rotation );
+    this.body.rotation.copy(this.dir);
+    console.log(this.body.rotation);
 };
 
 Ship.prototype.thrust = function(){
-    var x = speed * Math.cos(this.rot);
-    var z = speed * Math.sin(this.rot);
-    this.body.position.x += z;
-    this.body.position.z += x;
+    var dv = this.dir.clone();
+    dv.multiplyScalar(this.thrustForce);
+    this.vel.add(dv);
+
+    var len = this.vel.length();
+    if (len > this.maxSpeed) {
+        this.vel.normalize();
+        this.vel.multiplyScalar(this.maxSpeed);
+    }
 };
 
-/*
-// define the Student class
-function Student() {
-  // Call the parent constructor
-  Person.call(this);
-}
+Ship.prototype.step = function(){
+    this.body.position.add(this.vel);
+};
 
-// inherit Person
-Student.prototype = new Person();
-
-// correct the constructor pointer because it points to Person
-Student.prototype.constructor = Student;
- 
-// replace the sayHello method
-Student.prototype.sayHello = function(){
-  alert('hi, I am a student');
-}
-
-// add sayGoodBye method
-Student.prototype.sayGoodBye = function(){
-  alert('goodBye');
-}
-
-var student1 = new Student();
-student1.sayHello();
-student1.walk();
-student1.sayGoodBye();
-
-// check inheritance
-alert(student1 instanceof Person); // true 
-alert(student1 instanceof Student); // true
-*/
