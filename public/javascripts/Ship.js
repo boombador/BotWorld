073da5d2b59eeff9a2eb4a60b7d145c7560cb96c
Ian McLaughlin <ian.p.mclaughlin@gmail.com>
2013-07-06 05:07:31 -0400

    implemented loaders so you'll fly a proper ship. still need to work the kinks out of rotation, looks funky

--- public/javascripts/Ship.js
+++ public/javascripts/Ship.js
@@ -1,14 +1,12 @@
 
 function Ship() {
-    this.geoBody = new THREE.CubeGeometry(1,1,1);
-    this.matBody = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    this.body = new THREE.Mesh( this.geoBody, this.matBody );
 
     this.maxSpeed = .2;
     this.speed = 0;
     this.thrustForce = .0001;
     this.dir = new THREE.Vector3( 0, 0, 1 );
     this.vel = new THREE.Vector3( 0, 0, 0 );
+    this.loaded = false;
 
     return this;
 }
@@ -18,7 +16,7 @@ Ship.prototype.yaw = function(angle){
     var rotation = new THREE.Matrix4().makeRotationAxis( up, angle );
 
     this.dir.applyMatrix4( rotation );
-    this.body.rotation.copy(this.dir);
+    this.body.rotation.applyMatrix4( rotation );
 };
 
 Ship.prototype.thrust = function(){
@@ -35,5 +33,28 @@ Ship.prototype.thrust = function(){
 
 Ship.prototype.step = function(){
     this.body.position.add(this.vel);
+};
+
+Ship.prototype.loadMesh = function( geometry ) {
+    this.geoBody = geometry;
+    this.matBody = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    this.body = new THREE.Mesh( this.geoBody, this.matBody );
+
+    this.body.scale.set( .4, .4, .4 );
+    this.body.position.set( 0, 0, 0 );
+    this.body.rotation.set( 0, 0, 1 );
+    this.body.overdraw = true;
+
+    this.scene.add(this.body);
+    this.loaded = true;
+    console.log("Ship loaded");
+};
+
+Ship.prototype.init = function( loader, scene ){
+    this.scene = scene;
+    var that = this;
+    loader.load( "models/viper2.js", function(geometry){
+        that.loadMesh(geometry);
+    });
 };
 
--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -6,24 +6,14 @@ var mapRadius = 10;
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, 600 / 600, 0.1, 1000 );
 // var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-var renderer = new THREE.CanvasRenderer();
+// var renderer = new THREE.CanvasRenderer();
+var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 var loader = new THREE.JSONLoader();
-var createMesh = function( geometry )
-{
-    var zmesh = new THREE.Mesh( geometry, new THREE.MeshFaceMaterial() );
-    zmesh.position.set( 0, 0, 0 );
-    zmesh.overdraw = true;
-    scene.add( zmesh );
-    console.log(zmesh);
-};
-loader.load( "viper.js", createMesh );
-
 var ship = new Ship();
-var sbody = ship.body;
-scene.add( sbody );
+ship.init( loader, scene );
 
 var wall = new Wall();
 var wbody = wall.body;
@@ -31,30 +21,37 @@ scene.add( wbody );
 
 camera.position.y = 10;
 
+var state = 'loading';
 function render() {
-    if (sbody.position.length() > mapRadius) {
-        renderer.render(scene, camera);
-        return;
+    if (state == 'loading') {
+        if (ship.loaded) {
+            state = 'main';
+            console.log("game starting");
+        }
+    } else if (state == 'main') {
+        if (ship.body.position.length() > mapRadius) {
+            renderer.render(scene, camera);
+            return;
+        }
+        var x = 10 * Math.cos(camRot);
+        var z = 10 * Math.sin(camRot);
+        camera.position.x = x;
+        camera.position.z = z;
+        camera.lookAt(origin);
+
+        if( keyboard.pressed("left") ) {
+            ship.yaw(.01);
+        }
+        if( keyboard.pressed("right") ) {
+            ship.yaw(-.01);
+        }
+        if( keyboard.pressed("space") ) {
+            ship.thrust();
+        }
+        ship.step();
     }
+
     requestAnimationFrame(render);
-    var x = 10 * Math.cos(camRot);
-    var z = 10 * Math.sin(camRot);
-    camera.position.x = x;
-    camera.position.z = z;
-    camera.lookAt(origin);
-
-    if( keyboard.pressed("left") ) {
-        ship.yaw(.01);
-    }
-    if( keyboard.pressed("right") ) {
-        ship.yaw(-.01);
-    }
-    if( keyboard.pressed("space") ) {
-        ship.thrust();
-    }
-
-    ship.step();
-
     renderer.render(scene, camera);
 }
 render();
