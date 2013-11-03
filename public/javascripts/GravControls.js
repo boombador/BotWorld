c632d868a228855583e62694ced91d8aca0ea5b6
Ian McLaughlin <ian.p.mclaughlin@gmail.com>
2013-11-03 12:45:25 -0500

    add engine file abstraction

--- public/javascripts/GravControls.js
+++ public/javascripts/GravControls.js
@@ -2,99 +2,106 @@
 /* GravControls
  * heavily based off FlyControls by James Baicoianu / http://www.baicoianu.com/
  */
-THREE.GravControls = function ( entity ) {
 
-    this.keyboard = new THREEx.KeyboardState();
-    this.entity = entity;
+var gravControls = function( spec ) {
+    var that = {};
 
-    this.object = entity.mesh;
+    that.keyboard = new THREEx.KeyboardState();
 
-	this.dragToLook = false;
-	this.autoForward = false;
-	this.tmpQuaternion = new THREE.Quaternion();
-};
+    that.entity = spec.entity;
+    that.object = spec.entity.mesh;
 
-THREE.GravControls.prototype = {
+	that.dragToLook = false;
+	that.autoForward = false;
+	that.tmpQuaternion = new THREE.Quaternion();
 
-    constructor: THREE.GravControls,
+    that.movementSpeed = spec.movementSpeed || 10;
+    that.rollSpeed = spec.rollSpeed || Math.PI / 6;
+    that.autoForward = spec.autoForward || false;
+    that.dragToLook = spec.dragToLook || false;
 
-    rotate: function() {
+    that.rotate = function() {
         var angularThrust = {
             pitchUp: 0, pitchDown: 0,
             yawLeft: 0, yawRight: 0,
             rollLeft: 0, rollRight: 0
         };
         return function ( delta ) {
-            var rotMult = delta * this.accAngular;
-            angularThrust.pitchUp = this.keyboard.pressed( "up" ) ? 1 : 0;
-            angularThrust.pitchDown = this.keyboard.pressed( "down" ) ? 1 : 0;
-            angularThrust.yawLeft = this.keyboard.pressed( "left" ) ? 1 : 0;
-            angularThrust.yawRight = this.keyboard.pressed( "right" ) ? 1 : 0;
-            angularThrust.rollLeft = this.keyboard.pressed( "q" ) ? 1 : 0;
-            angularThrust.rollRight = this.keyboard.pressed( "e" ) ? 1 : 0;
+            var rotMult = delta * that.accAngular;
+            angularThrust.pitchUp = that.keyboard.pressed( "up" ) ? 1 : 0;
+            angularThrust.pitchDown = that.keyboard.pressed( "down" ) ? 1 : 0;
+            angularThrust.yawLeft = that.keyboard.pressed( "left" ) ? 1 : 0;
+            angularThrust.yawRight = that.keyboard.pressed( "right" ) ? 1 : 0;
+            angularThrust.rollLeft = that.keyboard.pressed( "q" ) ? 1 : 0;
+            angularThrust.rollRight = that.keyboard.pressed( "e" ) ? 1 : 0;
 
-            this.entity.rotationVector.x = ( -angularThrust.pitchDown + angularThrust.pitchUp );
-            this.entity.rotationVector.y = ( -angularThrust.yawRight  + angularThrust.yawLeft );
-            this.entity.rotationVector.z = ( -angularThrust.rollRight + angularThrust.rollLeft );
+            that.entity.rotationVector.x = ( -angularThrust.pitchDown + angularThrust.pitchUp );
+            that.entity.rotationVector.y = ( -angularThrust.yawRight  + angularThrust.yawLeft );
+            that.entity.rotationVector.z = ( -angularThrust.rollRight + angularThrust.rollLeft );
 
             // perform update
-            this.tmpQuaternion.set(
-                    this.entity.rotationVector.x * rotMult,
-                    this.entity.rotationVector.y * rotMult,
-                    this.entity.rotationVector.z * rotMult, 1
+            that.tmpQuaternion.set(
+                    that.entity.rotationVector.x * rotMult,
+                    that.entity.rotationVector.y * rotMult,
+                    that.entity.rotationVector.z * rotMult, 1
                     ).normalize();
-            this.object.quaternion.multiply( this.tmpQuaternion );
+            that.object.quaternion.multiply( that.tmpQuaternion );
 
             // expose the rotation vector for convenience
-            this.object.rotation.setEulerFromQuaternion( this.object.quaternion, this.object.eulerOrder );
+            that.object.rotation.setEulerFromQuaternion( that.object.quaternion, that.object.eulerOrder );
         };
 
     }(),
 
-    translate: function() {
+    that.translate = function() {
         var dv = new THREE.Vector3();
         var thrust = {
             up: 0, down: 0, left: 0, right: 0, forward: 0, back: 0,
         };
         return function ( delta ) {
-            var moveMult = delta * this.accLinear;
-            thrust.forward = this.keyboard.pressed( "w" ) ? 1 : 0;
-            thrust.back = this.keyboard.pressed( "s" ) ? 1 : 0;
-            thrust.left = this.keyboard.pressed( "a" ) ? 1 : 0;
-            thrust.right = this.keyboard.pressed( "d" ) ? 1 : 0;
-            thrust.up = this.keyboard.pressed( "r" ) ? 1 : 0;
-            thrust.down = this.keyboard.pressed( "f" ) ? 1 : 0;
+            var moveMult = delta * that.accLinear;
+            thrust.forward = that.keyboard.pressed( "w" ) ? 1 : 0;
+            thrust.back = that.keyboard.pressed( "s" ) ? 1 : 0;
+            thrust.left = that.keyboard.pressed( "a" ) ? 1 : 0;
+            thrust.right = that.keyboard.pressed( "d" ) ? 1 : 0;
+            thrust.up = that.keyboard.pressed( "r" ) ? 1 : 0;
+            thrust.down = that.keyboard.pressed( "f" ) ? 1 : 0;
 
             var forward = (
-                    thrust.forward || ( this.autoForward && !thrust.back )
+                    thrust.forward || ( that.autoForward && !thrust.back )
                     ) ? 1 : 0;
-            this.entity.moveVector.x = ( -thrust.left    + thrust.right );
-            this.entity.moveVector.y = ( -thrust.down    + thrust.up );
-            this.entity.moveVector.z = ( -forward + thrust.back );
+            that.entity.moveVector.x = ( -thrust.left    + thrust.right );
+            that.entity.moveVector.y = ( -thrust.down    + thrust.up );
+            that.entity.moveVector.z = ( -forward + thrust.back );
 
-            dv.copy( this.entity.moveVector );
-            dv.applyQuaternion( this.object.quaternion );
+            dv.copy( that.entity.moveVector );
+            dv.applyQuaternion( that.object.quaternion );
             dv.multiplyScalar( moveMult );
-            this.entity.vel.add( dv );
-            this.object.position.add( this.entity.vel );
+            that.entity.vel.add( dv );
+            that.object.position.add( that.entity.vel );
         };
     }(),
 
-    commandScan: function() {
-        var fireShot = this.keyboard.pressed( "space" ) ? 1 : 0;
+    that.commandScan = function() {
+        var fireShot = that.keyboard.pressed( "space" ) ? 1 : 0;
         var update;
-        // if (fireShot) update = this.entity.fireLaser( this.object.quaternion, this.object.eulerOrder );
+        // if (fireShot) update = that.entity.fireLaser( that.object.quaternion, that.object.eulerOrder );
         // else update = { type: 'none' };
         update = { type: 'none' };
         return update;
     },
 
-    update: function( delta ) {
+    that.update = function( delta ) {
         // update ship
-        this.translate( delta );
-        this.rotate( delta );
+        // console.log(that.entity.moveVector);
+        // console.log(that.entity.mesh.rotation);
+        that.entity;
+        debugger;
+        that.translate( delta );
+        that.rotate( delta );
 
-        return this.commandScan();
+        return that.commandScan();
     }
+    return that;
 };
 
